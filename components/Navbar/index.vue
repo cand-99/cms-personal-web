0ff2bde0000c1b860ae9f8d9da86545d06ca2d7f
candraherdiana <candraherdiana78@gmail.com>
2023-05-21 11:58:56 +0700

    fix: navbar

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -18,13 +18,15 @@ const handleLogout = async () => {
 </script>
 
 <template>
-  <nav ref="navbar" class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start xl:sticky mt-2">
+  <nav ref="navbar" class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start xl:sticky mt-2 z-3">
     <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
       <nav>
         <!-- breadcrumb -->
         <ol class="hidden md:flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
           <li class="leading-normal text-sm">
-            <a class="opacity-50 text-slate-700">Pages</a>
+            <p class="text-slate-500">
+              Pages
+            </p>
           </li>
           <li class="text-xs flex items-center ml-2">
             /
@@ -33,9 +35,9 @@ const handleLogout = async () => {
             {{ route.name }}
           </li>
         </ol>
-        <h6 class="mb-0 font-bold capitalize">
+        <h1 class="mb-0 font-bold capitalize">
           {{ route.name }}
-        </h6>
+        </h1>
       </nav>
 
       <div class="flex items-center justify-end grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
@@ -44,12 +46,43 @@ const handleLogout = async () => {
             <Icon name="ic:sharp-notifications" />
           </li>
           <li>
-            <Icon name="fa6-solid:gear" />
-          </li>
-          <li class="items-center hidden md:flex">
-            <button type="button" class="block px-0 py-2 font-semibold transition-all text-sm text-slate-500" @click="handleLogout()">
-              Logout
-            </button>
+            <ClientOnly>
+              <Menu as="div" class="relative inline-block text-left">
+                <div>
+                  <MenuButton aria-label="setting">
+                    <Icon name="ic:baseline-settings" />
+                  </MenuButton>
+                </div>
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+                >
+                  <MenuItems
+                    class="absolute right-0 mt-2 w-42 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <div class="px-1 py-1">
+                      <MenuItem v-slot="{ active }">
+                        <button
+                          class="group flex w-full items-center rounded-md px-2 py-2 text-sm transition-all" :class="[
+                            active ? 'bg-light-primary text-white' : 'text-slate-700',
+                          ]"
+                          @click="handleLogout()"
+                        >
+                          <Icon
+                            name="solar:logout-3-bold" size="20px" class="mr-2"
+                          />
+                          Logout
+                        </button>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </ClientOnly>
           </li>
           <li class="flex items-center xl:hidden">
             <button type="button" class="block p-1 transition-all text-2xl text-slate-500" @click="sideNav = !sideNav">
@@ -74,6 +107,5 @@ const handleLogout = async () => {
   --tw-shadow-colored: inset 0 0 1px 1px var(--tw-shadow-color), 0 20px 27px 0 var(--tw-shadow-color);
   box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
   backdrop-filter: blur(30px);
-  z-index: 980;
 }
 </style>
