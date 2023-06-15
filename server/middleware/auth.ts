dc56229950967edd44f913e30bb4df76ac5198cd
candraherdiana <candraherdiana78@gmail.com>
2023-06-15 18:38:19 +0700

    feat: API Middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,42 @@
+import UrlPattern from 'url-pattern'
+import { getSessionByAuthToken } from '../database/repositories/sessionRepository'
+export default defineEventHandler(async (event) => {
+  const isAllowed = await protectAuthRoute(event)
+
+  if (!isAllowed) {
+    deleteCookie(event, 'auth_token')
+    return sendError(event, createError({ statusCode: 401, statusMessage: 'Unauthorized' }))
+  }
+})
+
+async function protectAuthRoute(event: any): Promise<boolean> {
+  const token: any = getCookie(event, 'auth_token')
+
+  const protectedRoutes = [
+    '/api/project*',
+    '/api/technology*',
+    '/api/auth/logout',
+    '/api/auth/register*',
+  ]
+
+  const isHandledByThisMiddleware = protectedRoutes.some((endopoint) => {
+    const pattern = new UrlPattern(endopoint)
+
+    return pattern.match(event.path)
+  })
+
+  if (!event?.path || !isHandledByThisMiddleware)
+    return true
+
+  return await authCheck(token)
+}
+
+async function authCheck(token: string): Promise<boolean> {
+  if (!token)
+    return false
+  const user = await getSessionByAuthToken(token)
+  if (!user.user)
+    return false
+
+  return true
+}
