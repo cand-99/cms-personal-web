01717dd111d5fb99ba3d226ad4c249fd1213e1f0
candraherdiana <candraherdiana78@gmail.com>
2023-06-28 07:27:10 +0700

    fix: Access-Control-Allow-Origin

--- server/api/projects.get.ts
+++ server/api/projects.get.ts
@@ -12,16 +12,24 @@ export default defineEventHandler(async (event) => {
 
   const projects: IProject[] = await getActiveProjects(lang)
 
-  return projects.map(project => ({
-    id: project.id,
-    name: project.name,
-    description: project.description_id || project.description_en || project.description_ja || project.description_ko,
-    technology: project.technology,
-    dateStart: project.dateStart,
-    dateEnd: project.dateEnd,
-    mediaFiles: project.mediaFiles,
-    url: project.url,
-    isActive: project.isActive,
-    category: project.category,
-  } as IProject))
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    },
+    body: projects.map(project => ({
+      id: project.id,
+      name: project.name,
+      description: project.description_id || project.description_en || project.description_ja || project.description_ko,
+      technology: project.technology,
+      dateStart: project.dateStart,
+      dateEnd: project.dateEnd,
+      mediaFiles: project.mediaFiles,
+      url: project.url,
+      isActive: project.isActive,
+      category: project.category,
+    } as IProject)),
+  }
 })
